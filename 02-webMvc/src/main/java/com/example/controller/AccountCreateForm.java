bb7ce23c578ec4bfdd6fd4403081a32275cb68e8
bookstore <booookstore@icloud.com>
2018-06-17 14:55:42 +0900

    feat : フォームクラスを作成

---
+++ 02-webMvc/src/main/java/com/example/controller/AccountCreateForm.java
@@ -0,0 +1,29 @@
+package com.example.controller;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+public class AccountCreateForm implements Serializable {
+
+    private static final long serialVersionID = 1L;
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    private String name;
+
+    @NotNull
+    @Size(min = 9, max = 11)
+    private String tel;
+
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private String dateOfBirth;
+
+    @NotNull
+    @Size(min = 9, max = 256)
+    private String email;
+
+}
