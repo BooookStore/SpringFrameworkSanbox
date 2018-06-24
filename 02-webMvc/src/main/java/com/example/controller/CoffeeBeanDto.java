9f5f1d3dac15027303cbd9abbaafaad8db2304b6
bookstore <booookstore@icloud.com>
2018-06-24 17:51:42 +0900

    :sparkles: JSONでリクエストを返す様にして見た

--- 02-webMvc/src/main/java/com/example/controller/CoffeeBeanDto.java
+++ 02-webMvc/src/main/java/com/example/controller/CoffeeBeanDto.java
@@ -3,9 +3,26 @@ package com.example.controller;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 @Data
 @AllArgsConstructor
-public class CoffeeBeanDto {
+public class CoffeeBeanDto implements Serializable {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CoffeeBeanDto that = (CoffeeBeanDto) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 
     private String id;
 
--- 02-webMvc/src/main/java/com/example/controller/IndexController.java
+++ 02-webMvc/src/main/java/com/example/controller/IndexController.java
@@ -1,19 +1,24 @@
 package com.example.controller;
 
 import com.example.applicationservice.CoffeeBeanService;
+import com.example.domain.coffeebean.CoffeeBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
  * インデックスページのコントローラークラス。
  */
-@Controller
+@RestController
 public class IndexController {
 
     private final CoffeeBeanService coffeeBeanService;
@@ -26,16 +31,13 @@ public class IndexController {
     /**
      * インデックスページの表示
      */
-    @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String index(Model model) {
-        List<CoffeeBeanDto> coffeeBeanDtos = coffeeBeanService.getAllCoffeeBean().stream()
+    @GetMapping(path = "/")
+    public List<CoffeeBeanDto> index() {
+        return coffeeBeanService.getAllCoffeeBean().stream()
                 .map((b) -> new CoffeeBeanDto(
                         b.id().readable(),
                         b.name().fullName()))
                 .collect(Collectors.toList());
-
-        model.addAttribute("coffeeBeans", coffeeBeanDtos);
-        return "index";
     }
 
 }
